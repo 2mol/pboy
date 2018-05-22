e2bf46f9ecb8687f33a0211ce83777c2adb3bc8a
2mol <2mol@users.noreply.github.com>
2018-05-22 13:17:17 +0200

    function to parse a toml config file

--- src/Config.hs
+++ src/Config.hs
@@ -1,7 +1,14 @@
 module Config where
 
-import Lens.Micro.TH (makeLenses)
-import           System.FilePath         ((</>))
+import qualified Data.Text.IO     as TIO
+import qualified Data.Text     as T
+import           Lens.Micro.TH    (makeLenses)
+import qualified System.Directory as D
+import           System.FilePath  ((</>))
+import qualified Text.Toml        as Toml
+import           Data.Function           ((&))
+import           Text.Toml.Types  (Table)
+import Control.Arrow (left)
 
 data Config = Config
     { _inboxDir                   :: FilePath
@@ -17,6 +24,7 @@ data ImportAction
 
 makeLenses ''Config
 
+
 testDefaultConfig :: FilePath -> Config
 testDefaultConfig home =
     Config
@@ -26,3 +34,13 @@ testDefaultConfig home =
     , _nameSuggestionsMetaData = True
     , _importAction = Copy
     }
+
+readConfig :: IO (Either T.Text Table)
+readConfig = do
+    home <- D.getHomeDirectory
+    configTxt <- TIO.readFile (home </> ".pboy.toml")
+    let
+        configResult =
+            Toml.parseTomlDoc "" configTxt
+                & left (T.pack . Toml.parseErrorPretty)
+    pure configResult
