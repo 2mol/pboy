344ac51d024b57e852ce458b470228ca5d5813ab
tim put <timput@gmail.com>
2018-06-14 14:46:54 -0600

    Mechanically generated .nix files added

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ mkDerivation, base, brick, containers, directory, either
+, filepath, fmt, hpack, htoml-megaparsec, microlens, microlens-th
+, pdfinfo, process, stdenv, text, time, titlecase
+, unordered-containers, vector, vty
+}:
+mkDerivation {
+  pname = "pboy";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    base brick containers directory either filepath fmt
+    htoml-megaparsec microlens microlens-th pdfinfo process text time
+    titlecase unordered-containers vector vty
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    base brick containers directory either filepath fmt
+    htoml-megaparsec microlens microlens-th pdfinfo process text time
+    titlecase unordered-containers vector vty
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/2mol/pboy#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
