f1dd86198314f1a777b00546ba4a9850dc142509
2mol <2mol@users.noreply.github.com>
2018-10-17 17:33:22 +0200

    remove undefined in Config, hardcode default config for testing

--- src/Config.hs
+++ src/Config.hs
@@ -11,15 +11,14 @@ module Config
     , makeDefaultConfig
     ) where
 
-import           Control.Exception
-import           Data.HashMap.Lazy ((!))
+-- import           Control.Exception
+-- import           Data.HashMap.Lazy ((!))
 import qualified Data.Text         as T
 import qualified Data.Text.IO      as TIO
 import           Lens.Micro.TH     (makeLenses)
 import           Path              (Abs, Dir, File, Path, Rel, (</>))
 import qualified Path
-import           Path.IO
--- import qualified Text.Toml         as Toml
+import qualified Path.IO           as Path
 
 
 data Config = Config
@@ -35,75 +34,92 @@ data ImportAction
 makeLenses ''Config
 
 
+defaultConfig :: IO Config
+defaultConfig = do
+    home <- Path.getHomeDir
+    inbDir <- Path.resolveDir home $ "Downloads"
+    libDir <- Path.resolveDir home $ "papers"
+    pure Config
+        { _inboxDir = inbDir
+        , _libraryDir = libDir
+        , _importAction = Move
+        }
+
 getOrCreateConfig :: IO Config
-getOrCreateConfig = do
-    mconfig <- getConfig
-    case mconfig of
-        Just config -> pure config
-        Nothing -> do
-            makeDefaultConfig
-            getOrCreateConfig
+getOrCreateConfig = defaultConfig
+
+-- getOrCreateConfig :: IO Config
+-- getOrCreateConfig = do
+--     mconfig <- getConfig
+--     case mconfig of
+--         Just config -> pure config
+--         Nothing -> do
+--             makeDefaultConfig
+--             getOrCreateConfig
 
 
-getConfig :: IO (Maybe Config)
-getConfig = do
-    configHome <- getXdgDir XdgConfig Nothing
-    configTxtResult <- tryJust displayErr (TIO.readFile (Path.fromAbsFile (configHome </> defaultConfigFile)))
-    case configTxtResult of
-        Right configTxt -> do
-            let
-                configResult =
-                    undefined
-                    -- Toml.parseTomlDoc "" configTxt
+-- getConfig :: IO (Maybe Config)
+-- getConfig = do undefined
+    -- configHome <- Path.getXdgDir Path.XdgConfig Nothing
 
-            case configResult of
-                Left _ -> pure Nothing
-                Right configMap ->
-                    getConfigHelper configMap
+    -- let configPathStr = Path.fromAbsFile $ configHome </> defaultConfigFile
+
+    -- configTxtResult <-
+    --     tryJust displayErr (TIO.readFile configPathStr)
+
+    -- case configTxtResult of
+    --     Right configTxt -> do
+    --         let
+    --             configResult =
+    --                 -- Toml.parseTomlDoc "" configTxt
+
+    --         case configResult of
+    --             Left _ -> pure Nothing
+    --             Right configMap ->
+    --                 getConfigHelper configMap
 
 
-        Left _ ->
-            pure Nothing
+    --     Left _ ->
+    --         pure Nothing
 
 
-displayErr :: SomeException -> Maybe String
-displayErr e =
-    Just $ displayException e
+-- displayErr :: SomeException -> Maybe String
+-- displayErr e =
+--     Just $ displayException e
 
 
 -- getConfigHelper :: Toml.Table -> IO (Maybe Config)
-getConfigHelper configMap =
-    undefined
+-- getConfigHelper configMap =
     -- case (configMap ! "inbox", configMap ! "library", configMap ! "move") of
     --     (Toml.VString inb, Toml.VString lib, Toml.VBoolean mov) ->
     --         Just <$> configHelper inb lib mov
     --     _ -> pure Nothing
 
-configHelper :: T.Text -> T.Text -> Bool -> IO Config
-configHelper inb lib mov = do
-    let
-        act =
-            if mov
-                then Move
-                else Copy
-    home <- getHomeDir
-    inbd <- resolveDir home $ T.unpack inb
-    libd <- resolveDir home $ T.unpack lib
-    pure Config
-        { _inboxDir = inbd
-        , _libraryDir = libd
-        , _importAction = act
-        }
+-- configHelper :: T.Text -> T.Text -> Bool -> IO Config
+-- configHelper inb lib mov = do
+--     let
+--         act =
+--             if mov
+--                 then Move
+--                 else Copy
+--     home <- Path.getHomeDir
+--     inbDir <- Path.resolveDir home $ T.unpack inb
+--     libDir <- Path.resolveDir home $ T.unpack lib
+--     pure Config
+--         { _inboxDir = inbDir
+--         , _libraryDir = libDir
+--         , _importAction = act
+--         }
 
 makeDefaultConfig :: IO ()
 makeDefaultConfig = do
-    configHome <- getXdgDir XdgConfig Nothing
+    configHome <- Path.getXdgDir Path.XdgConfig Nothing
     TIO.writeFile (Path.fromAbsFile (configHome </> defaultConfigFile)) configContent
     where
         configContent =
             T.unlines
             [ "inbox = \"Downloads\""
-            , "library = \"pboy\""
+            , "library = \"papers\""
             , "move = true"
             ]
 
