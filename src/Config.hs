34494bd6b45ea3cae2c4e98479700e9aa4d3cd6a
2mol <2mol@users.noreply.github.com>
2018-10-21 16:01:51 +0200

    config parsing done

--- src/Config.hs
+++ src/Config.hs
@@ -13,17 +13,16 @@ module Config
     ) where
 
 import qualified Control.Exception as E
--- import           Data.HashMap.Lazy ((!))
 import           Data.Function ((&))
-import qualified Data.Text     as T
 import qualified Data.Text.IO  as TIO
 import           Lens.Micro.TH (makeLenses)
 import           Path          (Abs, Dir, File, Path, Rel, (</>))
 import qualified Path
 import qualified Path.IO       as Path
--- import Data.Ini.Config
-import           Data.Ini.Config.Bidir (Ini, IniSpec, (.=))
-import qualified Data.Ini.Config.Bidir as C
+import           Data.Ini.Config.Bidir   (Ini, IniSpec, (.=))
+import qualified Data.Ini.Config.Bidir   as C
+import           Lens.Micro              ((^.))
+
 
 data Config = Config
     { _inboxDir     :: Path Abs Dir
@@ -35,7 +34,6 @@ data ImportAction
     = Move | Copy
     deriving Show
 
-
 data ConfigData = ConfigData
     { _inboxDirD   :: FilePath
     , _libraryDirD :: FilePath
@@ -45,6 +43,7 @@ data ConfigData = ConfigData
 makeLenses ''Config
 makeLenses ''ConfigData
 
+
 defaultConfigData :: ConfigData
 defaultConfigData =
     ConfigData
@@ -53,11 +52,12 @@ defaultConfigData =
         , _importMove = True
         }
 
+
 createConfig :: IO Config
 createConfig = undefined
 
 
-tryGetConfig :: IO (Maybe Config)
+tryGetConfig :: IO (Either String Config)
 tryGetConfig = do
     configHome <- Path.getXdgDir Path.XdgConfig Nothing
 
@@ -66,22 +66,30 @@ tryGetConfig = do
     configTxtResult <-
         E.tryJust displayErr (TIO.readFile configPathStr)
 
-    case configTxtResult of
-        Right configTxt -> do
-            let
-                configResult = C.parseIni configTxt configIni
+    let
+        configIniResult =
+            configTxtResult >>= (\t -> C.parseIni t configIni)
 
-            case configResult of
-                Left _ -> pure Nothing
-                Right config ->
-                    Just <$> readConfigData (C.getIniValue config)
+        configResult =
+            C.getIniValue <$> configIniResult
 
+    sequence $ readConfigData <$> configResult
 
-        Left _ ->
-            pure Nothing
 
 readConfigData :: ConfigData -> IO Config
-readConfigData = undefined
+readConfigData configData = do
+    home <- Path.getHomeDir
+    inbDir <- Path.resolveDir home (configData ^. inboxDirD)
+    libDir <- Path.resolveDir home (configData ^. libraryDirD)
+
+    let action = if (configData ^. importMove) then Move else Copy
+
+    pure Config
+        { _inboxDir = inbDir
+        , _libraryDir = libDir
+        , _importAction = action
+        }
+
 
 configSpec :: IniSpec ConfigData ()
 configSpec = do
@@ -102,84 +110,14 @@ configSpec = do
                 \# If set to false it will leave the original file unchanged:"
                 ]
 
+
 configIni :: Ini ConfigData
 configIni = C.ini defaultConfigData configSpec
-
-
--- getOrCreateConfig :: IO Config
--- getOrCreateConfig = do
---     mconfig <- getConfig
---     case mconfig of
---         Just config -> pure config
---         Nothing -> do
---             makeDefaultConfig
---             getOrCreateConfig
-
-
--- getConfig :: IO (Maybe Config)
--- getConfig = do undefined
-    -- configHome <- Path.getXdgDir Path.XdgConfig Nothing
-
-    -- let configPathStr = Path.fromAbsFile $ configHome </> defaultConfigFile
-
-    -- configTxtResult <-
-    --     tryJust displayErr (TIO.readFile configPathStr)
-
-    -- case configTxtResult of
-    --     Right configTxt -> do
-    --         let
-    --             configResult =
-    --                 -- Toml.parseTomlDoc "" configTxt
-
-    --         case configResult of
-    --             Left _ -> pure Nothing
-    --             Right configMap ->
-    --                 getConfigHelper configMap
-
-
-    --     Left _ ->
-    --         pure Nothing
 
 
 displayErr :: E.SomeException -> Maybe String
 displayErr e =
     Just $ E.displayException e
-
-
--- getConfigHelper :: Toml.Table -> IO (Maybe Config)
--- getConfigHelper configMap =
-    -- case (configMap ! "inbox", configMap ! "library", configMap ! "move") of
-    --     (Toml.VString inb, Toml.VString lib, Toml.VBoolean mov) ->
-    --         Just <$> configHelper inb lib mov
-    --     _ -> pure Nothing
-
--- configHelper :: T.Text -> T.Text -> Bool -> IO Config
--- configHelper inb lib mov = do
---     let
---         act =
---             if mov
---                 then Move
---                 else Copy
---     home <- Path.getHomeDir
---     inbDir <- Path.resolveDir home $ T.unpack inb
---     libDir <- Path.resolveDir home $ T.unpack lib
---     pure Config
---         { _inboxDir = inbDir
---         , _libraryDir = libDir
---         , _importAction = act
---         }
-
--- makeDefaultConfig :: IO ()
--- makeDefaultConfig = do
---     configHome <- Path.getXdgDir Path.XdgConfig Nothing
---     TIO.writeFile (Path.fromAbsFile (configHome </> defaultConfigFile)) configContent
---     where
---         configContent =
---             T.unlines
---             [ "inbox = \"Downloads\""
---             , "library = \"papers\""
---             , "move = true"
---             ]
 
 
 defaultConfigFile :: Path Rel File
