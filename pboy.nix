713478643235fd5962dc683ac306827e1da98376
2mol <2mol@users.noreply.github.com>
2020-09-01 13:00:37 +0200

    upx and simplify

--- pboy.nix
+++ pboy.nix
@@ -9,7 +9,7 @@ let
   pboy = haskellPackages.callCabal2nix "pboy" (lib.cleanSource ./.) {};
 in
   symlinkJoin {
-    name = "pboy-1.1.0";
+    name = "pboy-1.4.0";
     buildInputs = [makeWrapper];
     postBuild = ''
       wrapProgram "$out/bin/pboy" \
